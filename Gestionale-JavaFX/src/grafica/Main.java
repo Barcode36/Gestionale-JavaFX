3f7767407a914d9a07cbb027a470ded4168e9ace
Francesco <francesco.espositoy@gmail.com>
2019-05-09 17:16:56 +0200

    Improvements

--- Gestionale-JavaFX/src/grafica/Main.java
+++ Gestionale-JavaFX/src/grafica/Main.java
@@ -1,5 +1,6 @@
 package grafica;
-	
+
+import logica.*;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
@@ -7,21 +8,31 @@ import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
 
 
-public class Main extends Application {
+public class Main extends Application 
+{
 	@Override
-	public void start(Stage primaryStage) {
-		try {
+	public void start(Stage primaryStage) 
+	{
+		try 
+		{
+			Gioielleria gioielleria = new Gioielleria("Francesco");
 			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Interfaccia.fxml"));
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			Scene scene = new Scene(root,1920,1080);
+			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setMinHeight(800);
+			primaryStage.setMinWidth(1000);
+			primaryStage.setTitle(gioielleria.getNomeGioielleria());
 			primaryStage.setScene(scene);
 			primaryStage.show();
-		} catch(Exception e) {
+		} 
+		catch(Exception e) 
+		{
 			e.printStackTrace();
 		}
 	}
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		launch(args);
 	}
 }
